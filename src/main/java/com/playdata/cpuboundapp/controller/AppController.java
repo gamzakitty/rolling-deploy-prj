e0780b677a765bda3bc0168cdc86b5a14cc00aa4
gamzakitty <96065039+gamzakitty@users.noreply.github.com>
2024-07-02 10:23:03 +0900

    Update AppController.java

--- src/main/java/com/playdata/cpuboundapp/controller/AppController.java
+++ src/main/java/com/playdata/cpuboundapp/controller/AppController.java
@@ -21,7 +21,7 @@ public class AppController {
 
     @RequestMapping("/hello")
     public String hello() {
-        return "webhook test restart!!!!! detach mode rolling deploy, sleep 60, 3, 4 instance only";
+        public String hello() { return "hello jenkins deploy";}
     }
 
     private String getMD5Digest(String input) throws NoSuchAlgorithmException {
