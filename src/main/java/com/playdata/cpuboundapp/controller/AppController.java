cb3765f86dadd0d779669175b86804d195b2580d
gamzakitty <96065039+gamzakitty@users.noreply.github.com>
2024-07-03 16:23:48 +0900

    Update AppController.java

--- src/main/java/com/playdata/cpuboundapp/controller/AppController.java
+++ src/main/java/com/playdata/cpuboundapp/controller/AppController.java
@@ -21,7 +21,7 @@ public class AppController {
 
     @RequestMapping("/hello")
     public String hello() {
-        return "오복이 귀엽다❤️";
+        return "오복이❤️";
     }
 
     private String getMD5Digest(String input) throws NoSuchAlgorithmException {
